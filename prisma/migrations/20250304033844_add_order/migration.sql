08a6e3234720656f94261cc8e0636c95e2d68d66
Avelimanuel <aveliemmanuel@gmail.com>
2025-03-04 06:44:34 +0300

    order & order items schema

---
+++ prisma/migrations/20250304033844_add_order/migration.sql
@@ -0,0 +1,41 @@
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "userId" UUID NOT NULL,
+    "shippingAddress" JSON NOT NULL,
+    "paymentMethod" TEXT NOT NULL,
+    "paymentResult" JSON,
+    "itemPrice" INTEGER NOT NULL DEFAULT 0,
+    "shippingPrice" INTEGER NOT NULL DEFAULT 0,
+    "taxPrice" INTEGER NOT NULL DEFAULT 0,
+    "totalPrice" INTEGER NOT NULL DEFAULT 0,
+    "isPain" BOOLEAN NOT NULL DEFAULT false,
+    "paidAt" TIMESTAMP(6),
+    "isDelivered" BOOLEAN NOT NULL DEFAULT false,
+    "deliveredAt" TIMESTAMP(6),
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderItem" (
+    "orderId" UUID NOT NULL,
+    "productId" UUID NOT NULL,
+    "qty" INTEGER NOT NULL,
+    "price" INTEGER NOT NULL DEFAULT 0,
+    "name" TEXT NOT NULL,
+    "Slug" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "orderitems_oredrId_productId_pk" PRIMARY KEY ("orderId","productId")
+);
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
